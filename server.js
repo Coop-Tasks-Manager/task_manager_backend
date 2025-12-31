cc7bec861d4d5e87e74cf2709532d3b73b88396d
tize1 <hermanotize@gmail.com>
2025-12-31 14:48:26 +0100

    server adj

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes");
