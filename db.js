bf5f81852c1a2ff9d77dfe088b54ebfc6d0a790e
tize1 <hermanotize@gmail.com>
2025-12-31 16:24:19 +0100

    ssl resolved

--- db.js
+++ db.js
@@ -15,6 +15,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: String(process.env.DB_PASSWORD), 
   port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
